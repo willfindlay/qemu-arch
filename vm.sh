affc6083687916fa21ae6bb5bb0b18f90f4e5b76
William Findlay <william@williamfindlay.com>
2021-01-29 18:25:29 -0500

    Update

--- vm.sh
+++ vm.sh
@@ -7,32 +7,34 @@ QEMU_ARCH=${QEMU_ARCH:-"x86_64"}
 MEM="${MEM:-4G}"
 CPU="${CPU:-4}"
 
-case "$1" in
-    start)
-        start_vm
-        ;;
-    livecd)
-        livecd_vm
-        ;;
-    ssh)
-        ssh_vm
-        ;;
-    kill)
-        kill_vm
-        ;;
-    *)
-        usage
-esac
+main() {
+    case "$1" in
+        start)
+            start_vm
+            ;;
+        livecd)
+            livecd_vm
+            ;;
+        ssh)
+            ssh_vm
+            ;;
+        kill)
+            kill_vm
+            ;;
+        *)
+            usage
+    esac
 
-exit 0
+    exit 0
+}
 
 start_vm() {
     qemu-system-$QEMU_ARCH \
         -net user,hostfwd=tcp::$PORT-:22 \
         -net nic \
         -m 256 \
-        -hda "$IMAGE" \
-        -display curses
+        -drive file="$IMAGE",format=raw \
+        -nographic
 }
 
 livecd_vm() {
@@ -42,12 +44,12 @@ livecd_vm() {
         -net user,hostfwd=tcp::$PORT-:22 \
         -net nic \
         -m 256 \
-        -hda "$IMAGE" \
-        -display curses
+        -drive file="$IMAGE",format=raw \
+        -nographic
 }
 
 ssh_vm() {
-    ssh -o $PORT root@localhost
+    ssh -p $PORT root@localhost
 }
 
 kill_vm() {
@@ -57,3 +59,5 @@ kill_vm() {
 usage() {
     echo "Usage: vm (start|livecd|ssh|kill)"
 }
+
+main "$@"
